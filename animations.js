d7ef8962d5016484388fd365b4aeeaba0e261cc8
OmnesEstUno <elliotjwarren@gmail.com>
2025-05-29 13:03:56 -0700

    js edit

--- animations.js
+++ animations.js
@@ -1,9 +1,9 @@
-    const imageContainer = document.querySelector('landing');
+    const image = document.getElementsByClassName('landing');
     let initialZoom = 1; // Initial scale factor
     const zoomSpeed = 0.01; // Amount to increase scale on each scroll
 
     document.addEventListener('scroll', () => {
         const scrollPosition = window.scrollY;
         const newZoom = initialZoom + scrollPosition * zoomSpeed;
-        imageContainer.style.transform = `scale(${newZoom})`;
+        image.style.transform = `scale(${newZoom})`;
     });
