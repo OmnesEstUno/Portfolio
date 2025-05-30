ed6ddcb96b3497c91e4e2a9ede5ce8ee3e9fa76b
OmnesEstUno <elliotjwarren@gmail.com>
2025-05-30 11:44:02 -0700

    js fix

--- animations.js
+++ animations.js
@@ -2,9 +2,11 @@ const image = document.getElementsByClassName('landing');
 let initialZoom = 1; // Initial scale factor
 const zoomSpeed = 0.01; // Amount to increase scale on each scroll
 
-document.addEventListener('scroll', () => {
-    const scrollPosition = window.scrollY;
-    console.log(scrollPosition);
-    const newZoom = initialZoom + scrollPosition * zoomSpeed;
-    image.style.transform = `scale(${newZoom})`;
-});
+if (image !== undefined && image !== null) {
+    document.addEventListener('scroll', () => {
+        const scrollPosition = window.scrollY;
+        console.log(scrollPosition);
+        const newZoom = initialZoom + scrollPosition * zoomSpeed;
+        image.style.transform = `scale(${newZoom})`;
+    });
+}
