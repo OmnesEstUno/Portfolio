1928f8054a5e28cc26dbcdca2564881303692ec8
OmnesEstUno <elliotjwarren@gmail.com>
2025-05-30 11:38:18 -0700

    js fix in html

--- animations.js
+++ animations.js
@@ -1,12 +1,10 @@
-function zoomCover() {
-    const image = document.getElementsByClassName('landing');
-    let initialZoom = 1; // Initial scale factor
-    const zoomSpeed = 0.01; // Amount to increase scale on each scroll
+const image = document.getElementsByClassName('landing');
+let initialZoom = 1; // Initial scale factor
+const zoomSpeed = 0.01; // Amount to increase scale on each scroll
 
-    document.addEventListener('scroll', () => {
-        const scrollPosition = window.scrollY;
-        console.log(scrollPosition);
-        const newZoom = initialZoom + scrollPosition * zoomSpeed;
-        image.style.transform = `scale(${newZoom})`;
-    });
-}
+document.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY;
+    console.log(scrollPosition);
+    const newZoom = initialZoom + scrollPosition * zoomSpeed;
+    image.style.transform = `scale(${newZoom})`;
+});
