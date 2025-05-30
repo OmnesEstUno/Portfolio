4633bff70cb7883b1b0a9a3f1dab101dbe5f8f50
OmnesEstUno <elliotjwarren@gmail.com>
2025-05-30 15:04:49 -0700

    js/css tweak

--- animations.js
+++ animations.js
@@ -1,14 +1,22 @@
 const image = document.getElementById('landing');
 let initialZoom = 1; // Initial scale factor
 const zoomSpeed = 0.001; // Amount to increase scale on each scroll
+var deltaY = 0;
+var initY = 0;
 
 if (image !== undefined && image !== null) {
-    ['wheel', 'touchmove'].forEach(function(event) {
+    ['wheel', 'touchstart', 'touchmove'].forEach(function(event) {
         document.addEventListener(event, () => {
-            const scrollPosition = WheelEvent.deltaY;
-            console.log(scrollPosition);
-            const newZoom = initialZoom + scrollPosition * zoomSpeed;
-            const newOpacity = initialZoom - scrollPosition * zoomSpeed;
+            if (event === "wheel"){
+                deltaY = event.deltaY;
+                console.log(deltaY);
+            } else if (event === "touchstart") {
+                initY = event.touches[0].pageY;
+            } else {
+                deltaY = start.y - event.touches[0].pageY;
+            }
+            const newZoom = initialZoom + deltaY * zoomSpeed;
+            const newOpacity = initialZoom - deltaY * zoomSpeed;
             image.style.transform = `scale(${newZoom});\nopacity: ${newOpacity}`;
         });
     })
