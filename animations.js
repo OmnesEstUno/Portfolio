81716bd2d99dcbed2a1ce0a395eebef028667f13
OmnesEstUno <elliotjwarren@gmail.com>
2025-05-30 16:02:05 -0700

    js test

--- animations.js
+++ animations.js
@@ -4,16 +4,15 @@
 const image = document.getElementById('landing');
 let initialZoom = 1; // Initial scale factor
 const zoomSpeed = 0.001; // Amount to increase scale on each scroll
-var deltaY = 0;
-var initY = 0;
 
 if (image !== undefined && image !== null) {
     ['wheel', 'touchstart', 'touchmove'].forEach(function(event) {
         document.addEventListener(event, () => {
+            var deltaY = 0;
+            var initY = 0;
             if (event === "wheel"){
-                var test = 0;
                 deltaY = event.deltaY;
-                console.log(test);
+                console.log(deltaY);
             } else if (event === "touchstart") {
                 initY = event.touches[0].pageY;
             } else {
