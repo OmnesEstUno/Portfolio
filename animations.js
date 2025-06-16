a693b52ffd41977c4ce0fe9d36912f0f4d01bf8c
OmnesEstUno <elliotjwarren@gmail.com>
2025-06-16 12:32:15 -0700

    js tweak

--- animations.js
+++ animations.js
@@ -11,32 +11,36 @@ const zoomSpeed = 0.005; // Amount to increase scale on each wheel event
 var newZoom = 0; // Value to set scale property to
 const opacitySpeed = 0.0005 // Amount to scale opacity on each wheel event
 var newOpacity = 0 // Value to set to opacity property to
-var deltaY = 0; // pixel distance "covered" per wheel event
+var deltaY = 0; // Pixel distance "covered" per wheel event
+var overscrollDeltaY = 0 // Placeholder to prevent overscroll calculations
 var initY = 0; // Start location for reference on mobile
 const targetW = 15; // Additional width to grant the name space to be 1 ln
 const targetX = 300; // Desktop X translation target
 const targetY = 260; // Desktop Y translation target
 
-if (image && nameTitle && resume && deltaY <= document.documentElement.scrollHeight) {
+if (image && nameTitle && resume) {
     ['wheel', 'touchstart', 'touchmove'].forEach(function(event) {
         document.addEventListener(event, (event) => {
+            if (event.deltaY <= 0 || event.deltaY >= document.documentElement.scrollHeight) {
+                overscrollDeltaY = event.deltaY;
+            }
             // Comparitor for the type of device used 
             // extrapolated from the screen width.
             // decides how to record the deltaY
             if(newOpacity >= 0){
                 if (event.type === "wheel"){
                     event.cancelable = false;
-                    deltaY += event.deltaY;
+                    deltaY += event.deltaY - overscrollDeltaY;
                 } else if (event.type === "touchstart") {
                     initY = event.touches[0].pageY;
                 } else if (event.type === 'touchmove') {
                     // Calculate delta for touch move
-                    deltaY += (initY - (event.touches[0]?.pageY || initY));
+                    deltaY += (initY - (event.touches[0]?.pageY || initY)) - overscrollDeltaY;
                 }
             }
 
             // Sets manipulation factor values where 0 is floor
-            if (deltaY <= 0){
+            if (deltaY <= 0){ // Indicates negative overscrolling; clamp at "top"
                 newZoom = 1;
                 newOpacity = 1;
             } else {
